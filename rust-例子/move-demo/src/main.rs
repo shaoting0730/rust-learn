4aa68d4ed3d39d5959813d10aa31f4f70dea6b45
zhoushaoting <510738319@qq.com>
2024-02-21 10:19:34 +0800

    修改move

--- rust-例子/move-demo/src/main.rs
+++ rust-例子/move-demo/src/main.rs
@@ -2,20 +2,20 @@
  * @Author: zhoushaoting 510738319@qq.com
  * @Date: 2024-02-02 12:56:55
  * @LastEditors: zhoushaoting 510738319@qq.com
- * @LastEditTime: 2024-02-21 09:46:05
+ * @LastEditTime: 2024-02-21 10:18:50
  * @FilePath: /undefined/Users/zhoushaoting/Desktop/GitHub/rust-learn/rust-例子/move-demo/src/main.rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 fn main() {
     let s = String::from("hello");
-    func_one(s); // 发生了move 传递String的所有权  String是在堆上保存，引用传递
+    func_one(s); // 发生了move 传递String的所有权  引用传递
     // println!("{}", s); // borrow of moved value: `s` value borrowed here after move
 
-    let x = 5; // 值传递 i32在栈上
+    let x = 5; // 基本类型 值传递 
     func_two(x);
     println!("{}", x);
 
-    let y = "str"; // 值传递 str在栈上
+    let y = "str"; // 字符切片 值传递 
     func_three(y);
     println!("{}", y);
 }

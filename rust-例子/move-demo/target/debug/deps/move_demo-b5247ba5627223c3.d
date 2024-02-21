5a132baa231e8119c7db06fd1bdad886b790a2ec
zhoushaoting <510738319@qq.com>
2024-02-21 09:47:28 +0800

    add move例子

---
+++ rust-例子/move-demo/target/debug/deps/move_demo-b5247ba5627223c3.d
@@ -0,0 +1,5 @@
+/Users/zhoushaoting/Desktop/GitHub/rust-learn/rust-例子/move-demo/target/debug/deps/libmove_demo-b5247ba5627223c3.rmeta: src/main.rs
+
+/Users/zhoushaoting/Desktop/GitHub/rust-learn/rust-例子/move-demo/target/debug/deps/move_demo-b5247ba5627223c3.d: src/main.rs
+
+src/main.rs:
---
+++ rust-例子/move-demo/target/debug/deps/move_demo-f3956e1bed69358a.d
@@ -0,0 +1,5 @@
+/Users/zhoushaoting/Desktop/GitHub/rust-learn/rust-例子/move-demo/target/debug/deps/libmove_demo-f3956e1bed69358a.rmeta: src/main.rs
+
+/Users/zhoushaoting/Desktop/GitHub/rust-learn/rust-例子/move-demo/target/debug/deps/move_demo-f3956e1bed69358a.d: src/main.rs
+
+src/main.rs:
